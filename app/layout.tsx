2c6230140246b1dda9ccd563b30f1f0f4e8d07c3
CSKFanbase07 <102754810+cskfanbase43606@users.noreply.github.com>
2025-05-18 13:19:17 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,8 +8,8 @@ import Footer from "@/components/main/Footer";
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'Nikhil Maguwala Portfolio',
-  description: 'Nikhil Maguwala\'s portfolio website',
+  title: 'Rahul Rana Portfolio',
+  description: 'Rahul Rana\'s portfolio website',
 }
 
 export default function RootLayout({
